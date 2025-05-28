48de357b0323377f26b836468e7bbc41f4774677
naturalnayasmetanka <marksmentina@gmail.com>
2025-05-28 21:42:46 +0300

    Refactor trimSymbols function to improve readability and maintainability

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  let newArr = [];
+  let counter = 0;
+  let lastSymbol = null;
+
+  for (let symbol of string) {
+    if (symbol === lastSymbol) {
+      counter += 1;
+    } else {
+      counter = 1;
+      lastSymbol = symbol;
+    }
+
+    if (counter <= size) {
+      newArr.push(symbol);
+    }
+  }
+
+  return newArr.join("");
 }
