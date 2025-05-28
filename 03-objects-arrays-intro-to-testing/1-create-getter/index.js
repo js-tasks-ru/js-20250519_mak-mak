9d902811a5b1cc59bf29b07291fc90df67e50217
naturalnayasmetanka <marksmentina@gmail.com>
2025-05-28 21:40:45 +0300

    Add getter function for extracting values from object by path
    - Implemented a new getter function that allows retrieving values from nested objects using a specified path.
    - The function supports dot notation and array indices, with error handling for invalid paths and undefined values.

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,25 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  let nodes = path.split(".");
+  let countIteration = 0;
 
+  return function getter(obj) {
+    let objKeys = Object.keys(obj);
+
+    if (objKeys.indexOf(nodes[countIteration]) === -1) {
+      return;
+    }
+    else if (countIteration === nodes.length - 1
+      && objKeys.indexOf(nodes[countIteration]) !== -1) {
+      let result = obj[nodes[countIteration]];
+      countIteration = 0;
+      return result;
+    }
+    else {
+      let innerObj = obj[nodes[countIteration]];
+      countIteration++;
+      return getter(innerObj);
+    }
+  };
 }
